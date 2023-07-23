6e62f38f87f33fa7c5f5275cf76d27c5dc36d986
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-07-23 11:56:32 -0700

    feat: create user and report entities using decorators from TypeORM

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
+import { ReportEntity } from './report.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReportEntity])],
   controllers: [ReportsController],
   providers: [ReportsService],
 })
