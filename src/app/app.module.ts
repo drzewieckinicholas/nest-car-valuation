d95c5e36e1926a80da8514555adbbfd04133b0f1
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-07-23 11:55:18 -0700

    feat: set up connection to database as a module to share across application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from 'src/reports/reports.module';
 import { UsersModule } from 'src/users/users.module';
+import { UserEntity } from 'src/users/user.entity';
+import { ReportEntity } from 'src/reports/report.entity';
 
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [UserEntity, ReportEntity],
+      synchronize: true,
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
